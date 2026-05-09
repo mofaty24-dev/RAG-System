7a93b85ba12280fe13953a397225da1602048ac8
fathy-dev <mofaty24@gmail.com>
2026-05-10 00:32:02 +0300

    Replace naive chunking with tokenization which is more reliable and adding sources for citation process
    Replace chunking process which messed up the text by uncomplete words and possible missing context along with refining text extraction from documents by separating each PDF with its name instead of merging PDFs with each other which will look messy to the LLM and make it hallucinate and finally adding citation property

--- Build_Index.py
+++ Build_Index.py
@@ -3,8 +3,10 @@ import pickle
 from pypdf import PdfReader
 import os
 from sentence_transformers import SentenceTransformer
+from transformers import AutoTokenizer
 
 model = SentenceTransformer("all-MiniLM-L6-v2")
+tokenizer = AutoTokenizer.from_pretrained("sentence-transformers/all-MiniLM-L6-v2")
 
 
 def read_pdf(path):
@@ -19,38 +21,40 @@ def read_pdf(path):
     return text
 
 
-def chunk_text(text, chunk_size=120, overlap=30):
-    words = text.split()
+def chunk_text(text,source_name, chunk_size=256, overlap=50 ):
+    tokenized_text = tokenizer.encode(text , add_special_tokens=False)
     chunks = []
-
     start = 0
-    while start < len(words):
-        end = min(start + chunk_size, len(words))
-        chunks.append(" ".join(words[start:end]))
+    while start < len(tokenized_text):
+        end = start + chunk_size
+        tokenized_chunk = tokenized_text[start:end]
+        chunk = tokenizer.decode(tokenized_chunk , skip_special_tokens=True)
+        chunks.append({"text":chunk ,"source_name":source_name})
         start += (chunk_size - overlap)
 
     return chunks
 
 
 def document_read(path):
-    all_text = ""
+    all_chunks = []
 
     for file in os.listdir(path):
         if file.endswith(".pdf"):
-            all_text += read_pdf(os.path.join(path, file))
-
-    return all_text
+            all_text = read_pdf(os.path.join(path, file))
+            chunks = chunk_text(text=all_text, source_name=file)
+            all_chunks.extend(chunks)
+    return all_chunks
 
 
 def embed_chunks(chunks):
-    embeddings = model.encode(chunks, convert_to_numpy=True)
-    faiss.normalize_L2(embeddings)
-    return embeddings
+    text = [chunk["text"] for chunk in chunks]
+    embedding = model.encode(text,convert_to_numpy=True).astype("float32")
+    faiss.normalize_L2(embedding)
+    return embedding
 
 
 def build_index(folder_path):
-    text = document_read(folder_path)
-    chunks = chunk_text(text)
+    chunks = document_read(folder_path)
     embeddings = embed_chunks(chunks)
 
     index = faiss.IndexFlatIP(embeddings.shape[1])
